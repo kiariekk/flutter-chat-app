a5ab1ccf444c9c33e725262cbe6975b1a3fd2880
marciovalim <marciogsvalim@gmail.com>
2020-12-12 13:05:38 -0300

    added cloud function for update last message

---
+++ firebase/functions/src/index.ts
@@ -0,0 +1,20 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+export const updateLastMessage = functions.firestore
+  .document("chats/{chatId}/messages/{messageId}")
+  .onCreate((snapshot, context) => {
+    const chatId = context.params.chatId;
+    const newMessageData = snapshot.data();
+    return admin
+      .firestore()
+      .doc(`chats/${chatId}`)
+      .update({
+        lastMessage: {
+          content: newMessageData.content,
+          createdAt: newMessageData.createdAt,
+        },
+      });
+  });
